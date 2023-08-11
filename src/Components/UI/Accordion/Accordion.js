d98942d37e6cc1eec8d450be96499d0f185382ea
Craig Bauer <craigbauer23@gmail.com>
2023-08-11 16:44:57 -0400

    added functional state update

--- src/Components/UI/Accordion/Accordion.js
+++ src/Components/UI/Accordion/Accordion.js
@@ -5,11 +5,21 @@ function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(-1);
 
   const updateIndex = (index) => {
-    if (index === expandedIndex) {
+    // get most uptodate piece of state (to avoid stale state reference)
+    // functional update when new value depends on old value
+    setExpandedIndex((current) => {
+      if (current === index) {
+        return -1;
+      } else {
+        return index;
+      }
+    });
+    // v Works fine here v
+    /* if (index === expandedIndex) {
       setExpandedIndex(-1);
     } else {
       setExpandedIndex(index);
-    }
+    } */
   };
 
   const renderedItems = items.map((item, index) => {
