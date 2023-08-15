3d724093b40ddbf0448394627cd53d2993fc081d
Craig Bauer <craigbauer23@gmail.com>
2023-08-15 19:45:19 -0400

    listen for clicks outside of dropdown to close when clicked

--- src/Components/UI/Dropdown/Dropdown.js
+++ src/Components/UI/Dropdown/Dropdown.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 import Panel from "../../Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdown = useRef();
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (!dropdown.current) {
+        return;
+      }
+      if (!dropdown.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handler, true); // set last arg to true to look for event handlers in the capture phase
+    return () => document.removeEventListener("click", handler);
+  }, []);
 
   const handleClick = () => {
     setIsOpen((current) => !current); // based on prev state
@@ -27,7 +41,7 @@ function Dropdown({ options, value, onChange }) {
   const icon = isOpen ? <GoChevronDown /> : <GoChevronLeft />;
 
   return (
-    <div className="w-48 relative">
+    <div className="w-48 relative" ref={dropdown}>
       <Panel
         className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
